5b6ead75beca7a1b97154c9c8417c565994fd66c
Reza <reza.bakhshi@lifelenz.com>
2023-12-01 10:06:27 +1030

    breaking sample

---
+++ MauiApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("MauiApp1")]
+namespace ClassLibrary1;
+
+internal class Class1
+{
+    public string GetValue()
+    {
+        return "value";
+    }
+}
+
--- MauiApp1/MauiApp1/App.xaml.cs
+++ MauiApp1/MauiApp1/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace MauiApp1
+﻿using ClassLibrary1;
+
+namespace MauiApp1
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
+            
+            Console.WriteLine(new Class1().GetValue());
+            
             MainPage = new AppShell();
         }
     }
